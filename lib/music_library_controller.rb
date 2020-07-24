23fd34598ea4fbf291038e52fb75f38392c50b09
wickbushong <wickbushong@gmail.com>
2020-07-24 08:59:18 -0400

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -12,27 +12,37 @@ class MusicLibraryController
     end
 
     def call
-        puts "Welcome to your music library!"
-        puts "To list all of your songs, enter 'list songs'."
-        puts "To list all of the artists in your library, enter 'list artists'."
-        puts "To list all of the genres in your library, enter 'list genres'."
-        puts "To list all of the songs by a particular artist, enter 'list artist'."
-        puts "To list all of the songs of a particular genre, enter 'list genre'."
-        puts "To play a song, enter 'play song'."
-        puts "To quit, type 'exit'."
-        puts "What would you like to do?"
-        input = gets.chomp
-        until input == "exit"
+        
+        input = ""
+        while input != "exit"
+            puts "Welcome to your music library!"
+            puts "To list all of your songs, enter 'list songs'."
+            puts "To list all of the artists in your library, enter 'list artists'."
+            puts "To list all of the genres in your library, enter 'list genres'."
+            puts "To list all of the songs by a particular artist, enter 'list artist'."
+            puts "To list all of the songs of a particular genre, enter 'list genre'."
+            puts "To play a song, enter 'play song'."
+            puts "To quit, type 'exit'."
+            puts "What would you like to do?"
+
             input = gets.chomp
-            # case input
-            # when "list songs"
-            #     Song.all.each{|song| puts song.name}
-            # when "list artists"
-            #     Artist.all.each{|artist| puts artist.name}
-            # when "list genres"
-            #     Genre.all.each{|genre| puts genre.name}
-            # end
+
+            case input
+            when "list songs"
+                list_songs
+            when "list artists"
+                list_artists
+            when "list genres"
+                list_genres
+            when "list artist"
+                list_songs_by_artist
+            when "list genre"
+                list_songs_by_genre
+            when "play song"
+                play_song
+            end
         end
+        
     end
 
     def list_songs
@@ -80,6 +90,15 @@ class MusicLibraryController
 
     def play_song
         puts "Which song number would you like to play?"
+        n = gets.chomp
+        if n.to_i > 0 && n.to_i <= Song.all.length
+            sorted = Song.all.sort_by{|song| song.name}
+            entry = sorted[n.to_i-1]
+
+            if entry
+                puts "Playing #{entry.name} by #{entry.artist.name}"
+            end
+        end
     end
 
 
