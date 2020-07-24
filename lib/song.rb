ccd488e2e6693a759c357ffb1621df21eece6989
Amanda_Katherine <walker.akw@gmail.com>
2020-07-24 14:32:15 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -11,6 +11,17 @@ class Song
         self.genre= genre unless genre == nil
     end
 
+    def self.new_from_filename(filename)
+        element = filename.split(" - ") 
+        artist = element[0]
+        name = element[1]
+        genre = element[3]
+        song = Song.new
+        song.artist = artist
+        song.name = name
+        song.genre = genre
+    end
+
     def self.find_by_name(name)
         self.all.find {|song| song.name == name}
     end
